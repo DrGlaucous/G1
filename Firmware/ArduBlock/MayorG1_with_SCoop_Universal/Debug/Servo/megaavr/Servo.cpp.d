475567984dbc00b82360e03d4f97b37accc1fbe4
Dr. Glaucous <49349519+DrGlaucous@users.noreply.github.com>
2022-05-09 00:08:57 -0400

    Delete Firmware/ArduBlock/MayorG1_with_SCoop_Universal/Debug directory
    IDE fodder

--- Firmware/ArduBlock/MayorG1_with_SCoop_Universal/Debug/Servo/megaavr/Servo.cpp.d
+++
@@ -1,2 +0,0 @@
-C:\Users\RemoteUser\AppData\Local\Temp\VMBuilds\MayorG1_with_SCoop_Universal\uno\Debug\Servo\megaavr\Servo.cpp.o: \
- C:\Program\ Files\ (x86)\Arduino\libraries\Servo\src\megaavr\Servo.cpp
--- Firmware/ArduBlock/MayorG1_with_SCoop_Universal/Debug/VM_DBG/VM_mem_check_sam.cpp.d
+++
@@ -1,2 +0,0 @@
-C:\Users\RemoteUser\AppData\Local\Temp\VMBuilds\MayorG1_with_SCoop_Universal\uno\Debug\VM_DBG\VM_mem_check_sam.cpp.o: \
- c:\program\ files\ (x86)\microsoft\ visual\ studio\2019\community\common7\ide\extensions\xdvejtee.cpu\Micro\ Platforms\default\debuggers\VM_DBG\VM_mem_check_sam.cpp

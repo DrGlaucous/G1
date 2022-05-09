7e474ca87f85065a95110ce4b17eebd133460c5f
Dr. Glaucous <49349519+DrGlaucous@users.noreply.github.com>
2022-05-09 00:09:10 -0400

    Delete Firmware/ArduBlock/MayorG1_with_SCoop_Universal/Release directory
    IDE fodder

--- Firmware/ArduBlock/MayorG1_with_SCoop_Universal/Release/Servo/nrf52/Servo.cpp.d
+++
@@ -1,2 +0,0 @@
-C:\Users\Edward\ Stuckey\AppData\Local\Temp\VMBuilds\MayorG1_with_SCoop_Universal\nano_atmega328\Release\Servo\nrf52\Servo.cpp.o: \
- C:\Program\ Files\ (x86)\Arduino\libraries\Servo\src\nrf52\Servo.cpp
